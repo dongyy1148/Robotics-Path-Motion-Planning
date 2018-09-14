7069210ade1a9b21a89346f7982a37011cae6cbf
董正 <dongzheng@dongzhengdeMacBook-Air.local>
2018-09-14 13:02:23 -0400

    update

--- tutorial_heapsort/heap.js
+++ tutorial_heapsort/heap.js
@@ -27,7 +27,17 @@ minheaper = {};
 
 // define insert function for min binary heap
 function minheap_insert(heap, new_element) {
-
+    heap.push(new_element);
+    x = heap.length-1;
+    while (x !== 0) {
+        y = Math.floor((x-1)/2);
+        if (heap[x] < heap[y]) {
+           a = heap[y];
+           heap[y] = heap[x];
+           heap[x] = a;
+        }
+        x = y;
+    }
     // STENCIL: implement your min binary heap insert operation
 }
 
@@ -40,12 +50,55 @@ minheaper.insert = minheap_insert;
 
 // define extract function for min binary heap
 function minheap_extract(heap) {
-
+    if (heap.length>1){
+        b = heap[0];
+        heap[0]=heap[heap.length-1];
+        heap[heap.length-1] = b;
+        x = 0;
+        y = 1;
+        while (y+2 < heap.length){
+            if (heap[y] < heap[y+1]){
+                if (heap[y] < heap[x]){
+                    a = heap[x];
+                    heap[x] = heap[y];
+                    heap[y] = a;
+                    x = y;
+                    y = 2*x+1;
+                    }
+                else {
+                    break
+                }    
+                }
+            else {
+                if (heap[y+1] < heap[x]){
+                    a = heap[x];
+                    heap[x] = heap[y+1];
+                    heap[y+1] = a;
+                    x = y+1;
+                    y = 2*x+1;
+                }
+                else {
+                    break
+                }
+            }
+        }
+        if (y+2 === heap.length){
+            if(heap[y] < heap[x]){
+                a = heap[x];
+                heap[x] = heap[y];
+                heap[y] = a;
+            }
+        }
+        return heap.pop()
+    }
+    else if (heap.length === 1){
+        return heap.pop();
+    }
     // STENCIL: implement your min binary heap extract operation
 }
 
 // assign extract function within minheaper object
-
+minheaper.extract = minheap_extract;
     // STENCIL: ensure extract method is within minheaper object
 
 
